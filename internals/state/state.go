1f8177f04ddf2536e61aa25c57e7e40e997bbe37
Rishik <rishik3555@gmail.com>
2026-03-23 16:20:42 +0530

    feat: add date update in file

---
+++ internals/state/state.go
@@ -0,0 +1,24 @@
+package state
+
+import (
+	"fmt"
+	"os"
+)
+
+type State struct {
+	Date 		string
+}
+
+func LastUpdated() string {
+	date, _ := os.ReadFile("last_updated.txt")
+	return string(date)
+}
+
+func UpdateLastUpdated(date string) {
+	err := os.WriteFile("last_updated.txt", []byte(date), 0644)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+	fmt.Println("formatted date", date)
+}
