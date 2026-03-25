7c57561ad41ac041675699843be0380d497e9ff5
Rishik <rishik3555@gmail.com>
2026-03-25 15:29:12 +0530

    chore: Replace placeholder URLs in `cmd/main.go` with actual values [skip ci]

--- cmd/main.go
+++ cmd/main.go
@@ -14,12 +14,17 @@ func main() {
 	if err != nil {
 		log.Println("Warning: No .env file found. Proceeding with existing environment variables.")
 	}
-
-	url := "https://arpitbhayani.me/rss.xml"
+	// TODO: Replace with actual RSS feed URL
+	var url string
+	//TODO: Replace with actual sender email and receiver email(s)
+	var senderEmail string
+	// TODO: Replace with actual receiver email(s)
+	var recieverEmail []string
 	result, err := rss.ParseRSSFeed(url)
 
 	if len(result)==0 {
 		fmt.Println("No new items found in the RSS feed.")
+		return
 	}
 
 	if err != nil {
@@ -28,7 +33,7 @@ func main() {
 	
 	for i:=range result {
 		item := result[i]
-		success, err1 := mailer.SendEmail("rishik3555@gmail.com",[]string{"rishik3555@gmail.com"}, item.Title, item.Link, item.PubDate, item.Description)
+		success, err1 := mailer.SendEmail(senderEmail,recieverEmail, item.Title, item.Link, item.PubDate, item.Description)
 		if err1 != nil {
 			fmt.Printf("Error sending email for item %s: %v\n", item.Title, err1)
 			return
